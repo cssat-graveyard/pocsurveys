e67ff32a1c2722fddd29a6c41cc66164bf319220
NETID\oieeri26 <oieeri26@uw.edu>
2018-05-15 15:17:58 -0700

    Made function more generalizable.

--- R/yes_no_function.R
+++ R/yes_no_function.R
@@ -4,25 +4,26 @@
 #'
 
 yes_no_function <- function(data, pretty = TRUE, useNA = "ifany", aggregate = TRUE){
-
+  if(any(class(data) == "factor")){
+    data <- as.character(data)
+  }
   if(pretty == TRUE){
-
-    if(any(class(dat1$cwtap_education) == "factor")){
-      data <- as.character(data)
-    }
-
-    if(any(str_detect(data, "Yes|No"), na.rm = TRUE) == FALSE){
+    if(!any(str_detect(data, "Yes|No"), na.rm = TRUE)){
       data <- ifelse(data == 0, "No", data)
       data <- ifelse(data == 1, "Yes", data)
     }
     data <- ifelse(is.na(data), "No Response", data)
     data <- factor(data, levels = c("Yes", "No", "No Response"))
   }
-
+  if(pretty == FALSE){
+    if(any(str_detect(data, "Yes|No"), na.rm = TRUE)){
+      data <- ifelse(data == "No", 0, data)
+      data <- ifelse(data == "Yes", 1, data)
+    }
+  }
   if(aggregate == TRUE){
     table(data, useNA = useNA)
   }else{
     return(data)
   }
-
 }
