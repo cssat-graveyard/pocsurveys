024417a2d08e971a4d9e00db29e9f78cf84bf7fe
Michelle Nguyen <mishn@uw.edu>
2018-06-08 13:06:21 -0700

    Adding dropdown function.

---
+++ R/dropdown_function.R
@@ -0,0 +1,11 @@
+dropdown_function <- function(data, ) {
+
+  data <- gather(data, item, response)
+
+  freq <- data %>%
+    group_by(item) %>%
+    count_by(item)
+
+  colnames(data) <- c("choice", "frequency")
+  return(data)
+}
